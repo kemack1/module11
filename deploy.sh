de2fd21172185bae1392fa6b5d601e6da4452738
Kahari Mack <kemack1@my.waketech.edu>
2026-04-06 21:55:20 -0400

    Complete Module 11 Vue Router assignment

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com:kemack1/vue-pages.git master:gh-pages
+git push -f git@github.com:kemack1/module11.git master:gh-pages
 
 cd -
